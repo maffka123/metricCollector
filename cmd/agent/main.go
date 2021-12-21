f643a533c167db28c3556ccd5f3c86c0b1ee2d26
maffka123 <mstihomirova@gmail.com>
2021-12-21 20:57:31 +0100

    added more tests and comments

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -17,26 +17,31 @@ var pollInterval time.Duration = 2    //s
 var reportInterval time.Duration = 10 //s
 
 func main() {
+	// for sending metrics to the server
 	client := &http.Client{}
-	//metricListChan := make(chan []collector.Metric)
 
-	metricList := initMetrics(client)
-
+	// To be able to cancel tasks, make ctx and signal handler
 	ctx, cancel := context.WithCancel(context.Background())
-
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
+	//starting list of metrices
+	metricList := initMetrics(client)
+
+	//tickers for metric update and metric sending to the server
 	pollTicker := time.NewTicker(pollInterval * time.Second)
 	reportTicker := time.NewTicker(reportInterval * time.Second)
 
+	//start both tasks
 	go updateMetrics(ctx, pollTicker.C, metricList)
 	go sendAllData(ctx, reportTicker.C, client, metricList)
 
+	//if signal to qiut received cancel ctx
 	<-quit
 	cancel()
 }
 
+//Initialize list with all metrics of interest, send first values to the server
 func initMetrics(client *http.Client) []*collector.Metric {
 	metricList := collector.GetAllMetrics()
 	for _, value := range metricList {
@@ -46,6 +51,7 @@ func initMetrics(client *http.Client) []*collector.Metric {
 	return metricList
 }
 
+//Update metrics from the list
 func updateMetrics(ctx context.Context, t <-chan time.Time, metricList []*collector.Metric) {
 	for {
 		select {
@@ -61,6 +67,7 @@ func updateMetrics(ctx context.Context, t <-chan time.Time, metricList []*collec
 	}
 }
 
+//Send metric to the server.
 func sendData(client *http.Client, m *collector.Metric) {
 	url := fmt.Sprintf("%s/update/%s/%s/%d", endpoint, m.Type, m.Name, m.Change.Value())
 	fmt.Println(url)
@@ -83,6 +90,7 @@ func sendData(client *http.Client, m *collector.Metric) {
 	defer response.Body.Close()
 }
 
+// Iterate over metrics list and sent them to the server
 func sendAllData(ctx context.Context, t <-chan time.Time, client *http.Client, metricList []*collector.Metric) {
 	for {
 
